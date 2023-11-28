91291e5dc48430013d2bd9bba686f1ffbf44a3cf
dappnodedev <diego@dappnode.io>
2023-11-28 17:36:32 +0100

    Allow multi-clustering

---
+++ charon-teku/entrypoint.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+# Exit on error
+set -eo pipefail
+
+#############
+# VARIABLES #
+#############
+ERROR="[ ERROR-charon-manager ]"
+WARN="[ WARN-charon-manager ]"
+INFO="[ INFO-charon-manager ]"
+
+CHARON_ROOT_DIR=/opt/charon/.charon
+CREATE_ENR_FILE=${CHARON_ROOT_DIR}/create_enr.txt
+ENR_PRIVATE_KEY_FILE=${CHARON_ROOT_DIR}/charon-enr-private-key
+ENR_FILE=${CHARON_ROOT_DIR}/enr
+DEFINITION_FILE_URL_FILE=${CHARON_ROOT_DIR}/definition_file_url.txt
+
+TEKU_SECURITY_DIR=/opt/charon/security
+TEKU_CERT_FILE=$TEKU_SECURITY_DIR/certs/teku_${CLUSTER_ID}_certificate.p12
+TEKU_CERT_PASS_FILE=$TEKU_SECURITY_DIR/certs/teku_certificate_pass.txt
+TEKU_CERT_PASS=$(cat $TEKU_CERT_PASS_FILE)
+
+CHARON_LOCK_FILE=${CHARON_ROOT_DIR}/cluster-lock.json
+REQUEST_BODY_FILE=${CHARON_ROOT_DIR}/request-body.json
+VALIDATOR_KEYS_DIR=${CHARON_ROOT_DIR}/validator_keys
+
+if [ -n "$DEFINITION_FILE_URL" ]; then
+  echo $DEFINITION_FILE_URL >$DEFINITION_FILE_URL_FILE
+fi
+
+export CHARON_P2P_EXTERNAL_HOSTNAME=${_DAPPNODE_GLOBAL_DOMAIN}
+
+#############
+# FUNCTIONS #
+#############
+
+function get_beacon_node_endpoint() {
+  case "$_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_HOLESKY" in
+  "prysm-holesky.dnp.dappnode.eth")
+    export CHARON_BEACON_NODE_ENDPOINTS="http://beacon-chain.prysm-holesky.dappnode:3500"
+    ;;
+  "teku-holesky.dnp.dappnode.eth")
+    export CHARON_BEACON_NODE_ENDPOINTS="http://beacon-chain.teku-holesky.dappnode:3500"
+    ;;
+  "lighthouse-holesky.dnp.dappnode.eth")
+    export CHARON_BEACON_NODE_ENDPOINTS="http://beacon-chain.lighthouse-holesky.dappnode:3500"
+    ;;
+  "nimbus-holesky.dnp.dappnode.eth")
+    export CHARON_BEACON_NODE_ENDPOINTS="http://beacon-validator.nimbus-holesky.dappnode:4500"
+    ;;
+  "lodestar-holesky.dnp.dappnode.eth")
+    export CHARON_BEACON_NODE_ENDPOINTS="http://beacon-chain.lodestar-holesky.dappnode:3500"
+    ;;
+  *)
+
+    export CHARON_BEACON_NODE_ENDPOINTS=$EXTERNAL_BEACON_NODE_ENDPOINT
+    echo "Using external beacon node endpoint: $CHARON_BEACON_NODE_ENDPOINTS"
+    ;;
+  esac
+}
+
+# Get the ENR of the node or create it if it does not exist
+function get_ENR() {
+  # Check if ENR file exists and create it if it does not
+  if [[ ! -f "$ENR_PRIVATE_KEY_FILE" ]]; then
+    echo "${INFO} ENR does not exist, creating it..."
+    charon create enr --data-dir=${CHARON_ROOT_DIR} | tee ${CREATE_ENR_FILE}
+  fi
+  # Get ENR from file
+  if [[ ! -f "$ENR" ]]; then
+    grep "enr:" ${CREATE_ENR_FILE} | cut -d " " -f 2 >$ENR_FILE
+    ENR=$(cat $ENR_FILE)
+    echo "${INFO} ENR: ${ENR}"
+    echo "${INFO} Publishing ENR to dappmanager..."
+    post_ENR_to_dappmanager
+  else
+    echo "${ERROR} it was not possible to get the ENR file"
+    sleep 300 # Wait 5 minutes to avoid restarting the container
+    exit 1
+  fi
+}
+
+# function to be post the ENR to dappmanager
+function post_ENR_to_dappmanager() {
+  # Post ENR to dappmanager
+  curl --connect-timeout 5 \
+    --max-time 10 \
+    --silent \
+    --retry 5 \
+    --retry-delay 0 \
+    --retry-max-time 40 \
+    -X POST "http://my.dappnode/data-send?key=ENR-Cluster-${CLUSTER_ID}&data=${ENR}" ||
+    {
+      echo "[ERROR] failed to post ENR to dappmanager"
+      exit 1
+    }
+}
+
+function check_DKG() {
+  # If the definition file URL is set and the lock file does not exist, start DKG ceremony
+  if [ -n "${DEFINITION_FILE_URL}" ] && [ ! -f "${CHARON_LOCK_FILE}" ]; then
+    echo "${INFO} Waiting for DKG ceremony..."
+    charon dkg --definition-file="${DEFINITION_FILE_URL}" --data-dir="${CHARON_ROOT_DIR}" || {
+      echo "${ERROR} DKG ceremony failed"
+      exit 1
+    }
+
+  # If the definition file URL is not set and the lock file does not exist, wait for the definition file URL to be set
+  elif [ -z "${DEFINITION_FILE_URL}" ] && [ ! -f "${CHARON_LOCK_FILE}" ]; then
+    echo "${INFO} Set the definition file URL in the Charon config to start DKG ceremony..."
+    exit 0
+
+  else
+    echo "${INFO} DKG ceremony already done. Process can continue..."
+  fi
+}
+
+function run_charon() {
+  # Start charon in a subshell in the background
+  (
+    exec charon run --private-key-file=$ENR_PRIVATE_KEY_FILE --lock-file=$CHARON_LOCK_FILE --builder-api
+  ) &
+}
+
+function run_teku_validator() {
+
+  exec /opt/teku/bin/teku --log-destination=CONSOLE \
+    validator-client \
+    --network=prater \
+    --beacon-node-api-endpoint=http://localhost:3600 \
+    --data-base-path=/opt/teku/data \
+    --metrics-enabled=true \
+    --metrics-interface 0.0.0.0 \
+    --metrics-port 8008 \
+    --metrics-host-allowlist=* \
+    --validator-api-enabled=false \
+    --validators-keystore-locking-enabled=false \
+    --validator-keys=${VALIDATOR_KEYS_DIR}:${VALIDATOR_KEYS_DIR} \
+    --network=${NETWORK} \
+    ${TEKU_EXTRA_OPTS}
+}
+
+########
+# MAIN #
+########
+
+echo "${INFO} get the current beacon chain in use"
+get_beacon_node_endpoint
+
+echo "${INFO} getting the ENR..."
+get_ENR
+
+echo "${INFO} checking for DKG ceremony..."
+check_DKG
+
+echo "${INFO} starting charon..."
+run_charon
+
+echo "${INFO} starting teku validator..."
+run_teku_validator
---
+++ getting-started.md
@@ -0,0 +1,35 @@
+# Obol Dappnode Package Guide
+
+The Obol Dappnode Package is designed to help users run a distributed validator setup with specific requirements. This guide explains the key aspects of the package and how to use it effectively.
+
+## Cluster Configuration
+
+1. **Maximum Clusters**: The package supports a maximum of 5 clusters.
+
+2. **Services**: Each cluster requires two dedicated services: Charon and Teku validator.
+
+## Getting the ENR
+
+3. **ENR for Clusters**: You will receive an ENR (Ethereum Name Record) for each cluster in the package. You can find these ENRs in the [Info tab](http://my.dappnode/#/packages/holesky-obol.dnp.dappnode.eth/info). These ENRs are needed to register each cluster as an operator before running the DKG (Distributed Key Generation) ceremony.
+
+4. **First Installation**: During the first installation, it's essential to leave all definition file inputs blank in the setup wizard. This allows you to obtain the ENR for each Charon node, which is necessary for the DKG process.
+
+## Managing Containers
+
+5. **Container State**: It's normal to see stopped containers, especially for clusters that are not currently active. The package is designed to stop containers for clusters that are not in use to save system resources.
+
+## Recovering an Existing Setup
+
+6. **Recovery Process**: If you are setting up or recovering an existing setup, you should provide the `definition-file-url` in the setup wizard. This URL should point to the definition file for your cluster, which you should have noted down after registering all the operators, performing the DKG ceremony, and running a command similar to the following:
+
+   ```bash
+   docker run --rm -v "$(pwd)/:/opt/charon" obolnetwork/charon:v0.12.0 dkg --definition-file="https://api.obol.tech/dv/0xf9632c4333e4d67373b777da56dfb764df47268881d3412a1eef1a0247dc7367/"
+   ```
+
+Replace the `definition-file-url` in the setup wizard with your cluster's specific URL in the following format:
+
+```markdown
+https://api.obol.tech/dv/0xf9632c4333e4d67373b383da56dfb764df47268881d3412a1eef1a0247dc7367
+```
+
+By following these steps and guidelines, you can effectively use the Obol Dappnode Package to manage your distributed validator clusters and participate in the DKG ceremony.
