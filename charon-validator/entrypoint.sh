d50343bc352648532450b26ddf14a6a422674936
dappnodedev <144998261+dappnodedev@users.noreply.github.com>
2024-04-16 17:12:36 +0200

    Support new compressed file format (#46)

--- charon-validator/entrypoint.sh
+++ charon-validator/entrypoint.sh
@@ -65,13 +65,35 @@ function move_old_charon() {
 # Extracts the import file into the .charon directory
 function extract_file_into_charon_dir() {
     echo "${INFO} Starting extraction of ${1} into ${CHARON_ROOT_DIR}"
+
+    # Create a temporary directory for initial extraction
+    tmp_dir=$(mktemp -d)
+
+    # Extract the archive to the temporary directory
     if [[ "${1}" == *.tar.gz ]]; then
-        tar -xzf "${1}" -C ${CHARON_ROOT_DIR} && echo "${INFO} Extraction complete."
+        tar -xzf "${1}" -C "${tmp_dir}" && echo "${INFO} Extraction to temp directory complete."
     elif [[ "${1}" == *.tar.xz ]]; then
-        tar -xJf "${1}" -C ${CHARON_ROOT_DIR} && echo "${INFO} Extraction complete."
+        tar -xJf "${1}" -C "${tmp_dir}" && echo "${INFO} Extraction to temp directory complete."
     elif [[ "${1}" == *.zip ]]; then
-        unzip -o "${1}" -d ${CHARON_ROOT_DIR} && echo "${INFO} Extraction complete."
+        unzip -o "${1}" -d "${tmp_dir}" && echo "${INFO} Extraction to temp directory complete."
     fi
+
+    # Check contents of the temp directory
+    contents=($(ls -A "${tmp_dir}"))
+    if [[ ${#contents[@]} == 1 && -d "${tmp_dir}/${contents[0]}" ]]; then
+        # If there is exactly one directory, move its contents to CHARON_ROOT_DIR
+        mv "${tmp_dir}/${contents[0]}"/* "${CHARON_ROOT_DIR}"
+        rmdir "${tmp_dir}/${contents[0]}" # Remove the now empty directory
+    else
+        # Move all files and directories from the temp directory directly to CHARON_ROOT_DIR
+        mv "${tmp_dir}"/* "${CHARON_ROOT_DIR}"
+    fi
+
+    echo "${INFO} Files moved to ${CHARON_ROOT_DIR}"
+
+    # Cleanup the temporary directory
+    rmdir "${tmp_dir}"
+    echo "${INFO} Temporary directory cleaned up."
 }
 
 # Remove all keys from the validator service
