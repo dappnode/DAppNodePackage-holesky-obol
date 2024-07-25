794ed05fa410e51a2dd473c4432115f57112a972
dappnodedev <diego@dappnode.io>
2024-07-25 13:02:27 +0200

    Optimize lodestar import

--- cluster/scripts/lodestar/keystore-import.sh
+++ cluster/scripts/lodestar/keystore-import.sh
@@ -5,29 +5,45 @@ INFO="[ INFO | lodestar-keystore-import ]"
 function import_keystores_to_lodestar() {
 
     local charon_keys_dir=${CHARON_ROOT_DIR}/validator_keys
-    local lodestar_keys_dir=${VALIDATOR_DATA_DIR}/keystores
+
+    local validator_keys_dir=${VALIDATOR_DATA_DIR}/keystores
+    local validator_secrets_dir=${VALIDATOR_DATA_DIR}/secrets
+
+    mkdir -p "${validator_keys_dir}" "${validator_secrets_dir}"
+
+    local imported_count=0
+    local existing_count=0
 
     for f in "${charon_keys_dir}"/keystore-*.json; do
+        local pubkey
+        local pubkey_dir
+        local password_file
 
-        # Read the JSON and get the pubkey field
-        pubkey=$(jq -r '.pubkey' "${f}")
+        echo "$INFO Importing key ${f}"
 
-        # Check if the keystore is already imported
-        if [[ -d "${lodestar_keys_dir}/0x${pubkey}" ]]; then
-            echo "$INFO Keystore for pubkey ${pubkey} already imported"
+        # Extract pubkey from keystore file
+        pubkey="0x$(grep '"pubkey"' "$f" | awk -F'"' '{print $4}')"
+        pubkey_dir="${validator_keys_dir}/${pubkey}"
 
-        else
-            echo "$INFO Importing key ${f}"
-
-            # Import keystore with password.
-            ${VALIDATOR_SERVICE_BIN} \
-                --dataDir="${VALIDATOR_DATA_DIR}" \
-                validator import \
-                --network="${NETWORK}" \
-                --importKeystores="${f}" \
-                --importKeystoresPassword="${f//json/txt}"
+        # Skip import if keystore already exists
+        if [[ -d "${pubkey_dir}" ]]; then
+            existing_count=$((existing_count + 1))
+            continue
         fi
+
+        mkdir -p "${pubkey_dir}"
+
+        # Copy the keystore file to persisted keys backend
+        install -m 600 "$f" "${pubkey_dir}/voting-keystore.json"
+
+        # Copy the corresponding password file
+        password_file="${f//json/txt}"
+        install -m 600 "${password_file}" "${validator_secrets_dir}/${pubkey}"
+
+        imported_count=$((imported_count + 1))
     done
+
+    echo "$INFO Processed all keys imported=${imported_count}, existing=${existing_count}, total=$(ls ${charon_keys_dir}/keystore-*.json | wc -l)"
 }
 
 import_keystores_to_lodestar
